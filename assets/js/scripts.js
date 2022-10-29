ffb0bbd0e6610d499002f69ec3009f961013ed4b
ryanrabello <ryan.s.rabello@gmail.com>
2022-10-28 20:10:23 -0700

    fix: removed froogaloop2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -217,25 +217,25 @@ jQuery(function ($) {
 
 
 
-    // -------------------------------------------------------------
-    // Vidio auto play
-    // -------------------------------------------------------------
-    (function () {
-
-    /* Vimeo API: http://developer.vimeo.com/player/js-api */
-
-        var iframe = document.getElementById('nofocusvideo');
-        // $f == Froogaloop
-        var player = $f(iframe);
-
-        $('.modal').on('hidden.bs.modal', function () {
-        player.api('pause');
-        })
-
-        $('.modal').on('shown.bs.modal', function () {
-        player.api('play');
-        })
-    }());
+    // // -------------------------------------------------------------
+    // // Vidio auto play
+    // // -------------------------------------------------------------
+    // (function () {
+    //
+    // /* Vimeo API: http://developer.vimeo.com/player/js-api */
+    //
+    //     var iframe = document.getElementById('nofocusvideo');
+    //     // $f == Froogaloop
+    //     var player = $f(iframe);
+    //
+    //     $('.modal').on('hidden.bs.modal', function () {
+    //     player.api('pause');
+    //     })
+    //
+    //     $('.modal').on('shown.bs.modal', function () {
+    //     player.api('play');
+    //     })
+    // }());
 
 
 
